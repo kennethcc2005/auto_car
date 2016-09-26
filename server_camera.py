f8fe83c641274676478f7f3657a2dc6bcffde2e4
Yumiko Huang <dayumikoda@gmail.com>
2016-09-25 21:05:24 -0700

    updated server_camera

--- server_camera.py
+++ server_camera.py
@@ -1,40 +1,49 @@
-import io 
-import socket
+import io
+import SocketServer
 import struct
 import time
 import picamera
 
-# create socket and bind host
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print '1'
-client_socket.connect(('192.168.1.104', 8000))
-print '2'
-connection = client_socket.makefile('wb')
-print '3'
+class MyTCPHandler(SocketServer.StreamRequestHandler):
+    """
+    The request handler class for our server.
 
-try:
-    print 'step1 ok'
-    with picamera.PiCamera() as camera:
-        print 'step2'
-        camera.resolution = (320, 240)      # pi camera resolution
-        camera.framerate = 10               # 10 frames/sec
-        time.sleep(2)                       # give 2 secs for camera to initilize
-        start = time.time()
-        stream = io.BytesIO()
-        
-        # send jpeg format video stream
-        for foo in camera.capture_continuous(stream, 'jpeg', use_video_port = True):
-            print 'step3 loop'
-            connection.write(struct.pack('<L', stream.tell()))
-            connection.flush()
-            stream.seek(0)
-            connection.write(stream.read())
-            if time.time() - start > 600:
-                break
-            stream.seek(0)
-            stream.truncate()
-    connection.write(struct.pack('<L', 0))
-finally:
-    print '4'
-    connection.close()
-    client_socket.close()
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        print 'step1 ok'
+        with picamera.PiCamera() as camera:
+            print 'step2'
+            camera.resolution = (320, 240)      # pi camera resolution
+            camera.framerate = 10               # 10 frames/sec
+            time.sleep(2)                       # give 2 secs for camera to initilize
+            start = time.time()
+            stream = io.BytesIO()
+
+            # send jpeg format video stream
+            for foo in camera.capture_continuous(stream, 'jpeg', use_video_port = True):
+                print 'step3 loop'
+                #self.wfile.write(struct.pack('<L', stream.tell()))
+                #self.flush()
+                #stream.seek(0)
+                #self.write(stream.read())
+                self.data = stream.read()
+                if time.time() - start > 600:
+                    break
+                stream.seek(0)
+                stream.truncate()
+        self.wfile.write(self.data.upper())
+
+if __name__ == "__main__":
+    HOST, PORT = "192.168.0.104", 9999
+
+    # Create the server, binding to localhost on port 9999
+    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
+
